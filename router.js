ef7b3623530037fb0517f5ea6cc873c2aa223467
Zhu-Peter <gzhu@ualberta.ca>
2024-05-15 19:10:51 -0600

    added router and views

---
+++ router.js
@@ -0,0 +1,22 @@
+import Vue from "vue";
+import Router from 'vue-router';
+import Home from './views/LoginView';
+import Game from './views/GameView'
+
+Vue.use(Router)
+
+const router = new Router({
+    mode: 'history',
+    routes: [
+        {
+            path: '/',
+            name: 'Home',
+            component: Home
+        },
+        {
+            path: '/game',
+            name: 'Game',
+            component: Game
+        }
+    ]
+})
